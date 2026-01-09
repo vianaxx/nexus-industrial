e11dfba7de92dcfc7dd1464582fed36fb06d19e4
vianaxx <carolvianaxx@gmail.com>
2026-01-08 22:41:50 -0300

    fix: Resolve utils module import conflict by moving legacy functions to utils/legacy.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,19 +2,36 @@
 Utilities module for Nexus Industrial Intelligence.
 """
 
+# Legacy utility functions (from old utils.py)
+from .legacy import (
+    format_cnpj,
+    format_currency,
+    format_date,
+    get_status_description,
+    format_cnae
+)
+
+# New Brazilian number formatters (ABNT NBR 5891)
 from .formatters import (
     format_br_number,
     format_count,
-    format_currency,
+    format_currency as format_currency_br,
     format_percentage,
     format_index,
     format_altair_axis
 )
 
 __all__ = [
+    # Legacy functions
+    'format_cnpj',
+    'format_currency',
+    'format_date',
+    'get_status_description',
+    'format_cnae',
+    # New formatters
     'format_br_number',
     'format_count',
-    'format_currency',
+    'format_currency_br',
     'format_percentage',
     'format_index',
     'format_altair_axis'
